560da4a0cadffd6c0cfd4c054d96e761f0f42f94
WyldePointer <sohrab.monfared@gmail.com>
2017-09-07 18:20:24 +0430

    Initial commit.

---
+++ snakes_and_ladders.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int       ma\
+in(       vo\
+id){int i,j,\
+k,x       ;i\
+=j=       k=\
+x=0       ;f\
+or(i=10;i>=1\
+;i--      ){\
+x=(       i%\
+2)?9:0;for(j\
+=10       ,k\
+=x;       j>\
+=1;j--){prin\
+tf(       "%\
+3.d "     ,(\
+i*10)-k);k=(\
+i%2       )?\
+k-1       :k\
++1;       }p\
+rintf("\n");}
+return 0+00;}
