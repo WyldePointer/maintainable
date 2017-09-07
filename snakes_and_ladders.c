88abc43fc736282294667343fa160d962c38fc9d
WyldePointer <sohrab.monfared@gmail.com>
2017-09-07 20:04:20 +0430

    Improved.

--- snakes_and_ladders.c
+++ snakes_and_ladders.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
+#define dsp printf
 
-int       ma\
-in(       vo\
-id){int i,j,\
-k,x       ;i\
-=j=       k=\
-x=0       ;f\
-or(i=10;i>=1\
-;i--      ){\
-x=(       i%\
-2)?9:0;for(j\
-=10       ,k\
-=x;       j>\
-=1;j--){prin\
-tf(       "%\
-3.d "     ,(\
-i*10)-k);k=(\
-i%2       )?\
-k-1       :k\
-+1;       }p\
-rintf("\n");}
-return 0+00;}
+int    ma\
+in(    vo\
+id){int i\
+,j,k,x;i=\
+j=k    =x\
+=0;    fo\
+r(i=10;i>\
+=1;i--){x\
+=(i    %2\
+)?9    :0\
+;for(j=10\
+,k=x;j>=1\
+;j--   ){\
+dsp    ("\
+%3.d",(i*\
+10)-k);k=\
+(i%    2)\
+?k-    1:\
+k+1;}dsp(\
+"\n");}re\
+turn   0;}
+/**    **/
